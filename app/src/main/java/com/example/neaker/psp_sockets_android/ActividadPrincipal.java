d1269204211387ba933467646bf916435a147dd7
neaker01 <snakeone93@hotmail.com>
2019-01-28 13:20:44 +0100

    seg commit

---
+++ app/src/main/java/com/example/neaker/psp_sockets_android/ActividadPrincipal.java
@@ -0,0 +1,17 @@
+package com.example.neaker.psp_sockets_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActividadPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_principal);
+
+
+
+
+    }
+}
